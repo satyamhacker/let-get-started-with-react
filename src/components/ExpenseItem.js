fb5bae837d79f047a96a0f9d576f89598fd8a767
satyam Singh <55867907+satyamhacker@users.noreply.github.com>
2023-10-13 19:19:39 +0530

    Task-9
    Task-9

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,25 @@
-function ExpenseItem() {
-  return (
-    <div>
-      <h2>expense items</h2>
-      <div>food rs 10</div>
-    </div>
-  );
+import './ExpenseItem.css';
+
+
+
+
+
+function ExpenseItem(props){
+
+    return(
+        <div>
+            <h2>expense items</h2>
+            <div>{props.date.toISOString()}</div>
+            <div className ="expense-item">
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>Rs { props.amount}</div>
+            </div>
+        </div>
+
+        </div>
+
+    ) 
 }
 
 export default ExpenseItem;
