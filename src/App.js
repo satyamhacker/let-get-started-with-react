1d71068153a810213c0939c76c8cdca09e989211
satyam Singh <55867907+satyamhacker@users.noreply.github.com>
2023-10-13 17:35:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+
+    <div>
+
+      <ExpenseItem/>
+
+
+      <h1>test</h1>
+
+
+
+    </div>
+    
+
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,10 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>expense items</h2>
+      <div>food rs 10</div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
